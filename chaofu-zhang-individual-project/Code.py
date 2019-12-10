0cdfcc46b6dcf6a79e5a61e2401364cc417629a0
tiandao4414 <42441485+tiandao4414@users.noreply.github.com>
2019-12-09 23:02:00 -0500

    Add files via upload

---
+++ chaofu-zhang-individual-project/Code.py
@@ -0,0 +1,323 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import cv2
+from tqdm import tqdm#_notebook as tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from functools import reduce
+import os
+from scipy.optimize import minimize
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+from torchvision import transforms, utils
+
+PATH = '/content/drive/My Drive/Pku Kaggle/'
+os.listdir(PATH)
+
+IMG_SHAPE = [2710, 3384]
+BATCH_SIZE = 4
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_epochs = 10
+
+train = pd.read_csv(PATH + 'train.csv')
+test = pd.read_csv(PATH + 'sample_submission.csv')
+# From camera.zip
+camera_matrix = np.array([[2304.5479, 0,  1686.2379],
+                          [0, 2305.8757, 1354.9849],
+                          [0, 0, 1]], dtype=np.float32)
+camera_matrix_inv = np.linalg.inv(camera_matrix)
+
+def imread(path, fast_mode=False):
+    img = cv2.imread(path)
+    if not fast_mode and img is not None and len(img.shape) == 3:
+        img = np.array(img[:, :, ::-1])
+    return img
+
+def str2coords(s, names=['id', 'yaw', 'pitch', 'roll', 'x', 'y', 'z']):
+    '''
+    Input:
+        s: PredictionString (e.g. from train dataframe)
+        names: array of what to extract from the string
+    Output:
+        list of dicts with keys from `names`
+    '''
+    coords = []
+    for l in np.array(s.split()).reshape([-1, 7]):
+        coords.append(dict(zip(names, l.astype('float'))))
+        if 'id' in coords[-1]:
+            coords[-1]['id'] = int(coords[-1]['id'])
+    return coords
+
+points_df = pd.DataFrame()
+for col in ['x', 'y', 'z', 'yaw', 'pitch', 'roll']:
+    arr = []
+    for ps in train['PredictionString']:
+        coords = str2coords(ps)
+        arr += [c[col] for c in coords]
+    points_df[col] = arr
+
+print('len(points_df)', len(points_df))
+
+def rotate(x, angle):
+    x = x + angle
+    x = x - (x + np.pi) // (2 * np.pi) * 2 * np.pi
+    return x
+
+def get_img_coords(s):
+    '''
+    Input is a PredictionString (e.g. from train dataframe)
+    Output is two arrays:
+        xs: x coordinates in the image (row)
+        ys: y coordinates in the image (column)
+    '''
+    coords = str2coords(s)
+    xs = [c['x'] for c in coords]
+    ys = [c['y'] for c in coords]
+    zs = [c['z'] for c in coords]
+    P = np.array(list(zip(xs, ys, zs))).T
+    img_p = np.dot(camera_matrix, P).T
+    img_p[:, 0] /= img_p[:, 2]
+    img_p[:, 1] /= img_p[:, 2]
+    img_xs = img_p[:, 0]
+    img_ys = img_p[:, 1]
+    img_zs = img_p[:, 2] # z = Distance from the camera
+    return img_xs, img_ys
+
+from math import sin, cos
+
+# convert euler angle to rotation matrix
+def euler_to_Rot(yaw, pitch, roll):
+    Y = np.array([[cos(yaw), 0, sin(yaw)],
+                  [0, 1, 0],
+                  [-sin(yaw), 0, cos(yaw)]])
+    P = np.array([[1, 0, 0],
+                  [0, cos(pitch), -sin(pitch)],
+                  [0, sin(pitch), cos(pitch)]])
+    R = np.array([[cos(roll), -sin(roll), 0],
+                  [sin(roll), cos(roll), 0],
+                  [0, 0, 1]])
+    return np.dot(Y, np.dot(P, R))
+
+def draw_line(image, points):
+    color = (255, 0, 0)
+    cv2.line(image, tuple(points[0][:2]), tuple(points[3][:2]), color, 16)
+    cv2.line(image, tuple(points[0][:2]), tuple(points[1][:2]), color, 16)
+    cv2.line(image, tuple(points[1][:2]), tuple(points[2][:2]), color, 16)
+    cv2.line(image, tuple(points[2][:2]), tuple(points[3][:2]), color, 16)
+    return image
+
+
+def draw_points(image, points):
+    for (p_x, p_y, p_z) in points:
+        cv2.circle(image, (p_x, p_y), int(1000 / p_z), (0, 255, 0), -1)
+#         if p_x > image.shape[1] or p_y > image.shape[0]:
+#             print('Point', p_x, p_y, 'is out of image with shape', image.shape)
+    return image
+
+IMG_WIDTH, IMG_HEIGHT = 2048, 640
+MODEL_SCALE = 8
+
+
+def _regr_preprocess(regr_dict, flip=False):
+    if flip:
+        for k in ['x', 'pitch', 'roll']:
+            regr_dict[k] = -regr_dict[k]
+    for name in ['x', 'y', 'z']:
+        regr_dict[name] = regr_dict[name] / 100
+    regr_dict['roll'] = rotate(regr_dict['roll'], np.pi)
+    regr_dict['pitch_sin'] = sin(regr_dict['pitch'])
+    regr_dict['pitch_cos'] = cos(regr_dict['pitch'])
+    regr_dict.pop('pitch')
+    regr_dict.pop('id')
+    return regr_dict
+
+
+def _regr_back(regr_dict):
+    for name in ['x', 'y', 'z']:
+        regr_dict[name] = regr_dict[name] * 100
+    regr_dict['roll'] = rotate(regr_dict['roll'], -np.pi)
+
+    pitch_sin = regr_dict['pitch_sin'] / np.sqrt(regr_dict['pitch_sin'] ** 2 + regr_dict['pitch_cos'] ** 2)
+    pitch_cos = regr_dict['pitch_cos'] / np.sqrt(regr_dict['pitch_sin'] ** 2 + regr_dict['pitch_cos'] ** 2)
+    regr_dict['pitch'] = np.arccos(pitch_cos) * np.sign(pitch_sin)
+    return regr_dict
+
+
+
+
+def preprocess_image(img, flip=False):
+    img = img[img.shape[0] // 2:]
+    bg_v = np.ones_like(img) * img.mean(1, keepdims=True).astype(img.dtype)
+    bg_l = bg_v[:, :img.shape[1] // 8]
+    bg_r = bg_v[:, :int(img.shape[0] * 3.2) - img.shape[1] // 8 - img.shape[1]]
+
+    img = np.concatenate([bg_l, img, bg_r], 1)
+
+    # img = img[img.shape[0] // 2:]
+    # bg = bg[:img.shape[1] - img.shape[0], :]
+    # img = np.concatenate([img, bg_down], 0)
+    #print(img.shape)
+    img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+    if flip:
+        img = img[:, ::-1]
+    return (img / 255).astype('float32')
+
+
+def get_mask_and_regr(img, labels, flip=False):
+    mask = np.zeros([IMG_HEIGHT // MODEL_SCALE, IMG_WIDTH // MODEL_SCALE], dtype='float32')
+    regr_names = ['x', 'y', 'z', 'yaw', 'pitch', 'roll']
+    regr = np.zeros([IMG_HEIGHT // MODEL_SCALE, IMG_WIDTH // MODEL_SCALE, 7], dtype='float32')
+    coords = str2coords(labels)
+    xs, ys = get_img_coords(labels)
+    for x, y, regr_dict in zip(xs, ys, coords):
+        x_scale, y_scale = y, x
+        x_scale = (x_scale - img.shape[0] // 2) * IMG_HEIGHT / (img.shape[0] // 2) / MODEL_SCALE
+        x_scale = np.round(x_scale).astype('int')
+        y_scale = (y_scale + img.shape[1] // 8) * IMG_WIDTH / (img.shape[0] * 16 / 5) / MODEL_SCALE
+        y_scale = np.round(y_scale).astype('int')
+        if x_scale >= 0 and x_scale < IMG_HEIGHT // MODEL_SCALE and y_scale >= 0 and y_scale < IMG_WIDTH // MODEL_SCALE:
+            mask[x_scale, y_scale] = 1
+            regr_dict = _regr_preprocess(regr_dict, flip)
+            regr[x_scale, y_scale] = [regr_dict[n] for n in sorted(regr_dict)]
+    if flip:
+        mask = np.array(mask[:, ::-1])
+        regr = np.array(regr[:, ::-1])
+    return mask, regr
+
+
+DISTANCE_THRESH_CLEAR = 2
+
+
+def convert_3d_to_2d(x, y, z, fx=2304.5479, fy=2305.8757, cx=1686.2379, cy=1354.9849):
+    # borrowed from https://www.kaggle.com/theshockwaverider/eda-visualization-baseline
+    return x * fx / z + cx, y * fy / z + cy
+
+zy_slope = LinearRegression()
+X = points_df[['z']]
+y = points_df['y']
+zy_slope.fit(X, y)
+
+xzy_slope = LinearRegression()
+X = points_df[['x', 'z']]
+y = points_df['y']
+xzy_slope.fit(X, y)
+
+def optimize_xy(r, c, x0, y0, z0, flipped=False):
+    def distance_fn(xyz):
+        x, y, z = xyz
+        xx = -x if flipped else x
+        slope_err = (xzy_slope.predict([[xx, z]])[0] - y) ** 2
+        x, y = convert_3d_to_2d(x, y, z)
+        y, x = x, y
+        x = (x - IMG_SHAPE[0] // 2) * IMG_HEIGHT / (IMG_SHAPE[0] // 2) / MODEL_SCALE
+        y = (y + IMG_SHAPE[1] // 8) * IMG_WIDTH / (IMG_SHAPE[1] * 4 / 3) / MODEL_SCALE
+        return max(0.2, (x - r) ** 2 + (y - c) ** 2) + max(0.4, slope_err)
+
+    res = minimize(distance_fn, [x0, y0, z0], method='Powell')
+    x_new, y_new, z_new = res.x
+    return x_new, y_new, z_new
+
+
+def clear_duplicates(coords):
+    for c1 in coords:
+        xyz1 = np.array([c1['x'], c1['y'], c1['z']])
+        for c2 in coords:
+            xyz2 = np.array([c2['x'], c2['y'], c2['z']])
+            distance = np.sqrt(((xyz1 - xyz2) ** 2).sum())
+            if distance < DISTANCE_THRESH_CLEAR:
+                if c1['confidence'] < c2['confidence']:
+                    c1['confidence'] = -1
+    return [c for c in coords if c['confidence'] > 0]
+
+
+def extract_coords(prediction, flipped=False):
+    logits = prediction[0]
+    regr_output = prediction[1:]
+    points = np.argwhere(logits > 0)
+    col_names = sorted(['x', 'y', 'z', 'yaw', 'pitch_sin', 'pitch_cos', 'roll'])
+    coords = []
+    for r, c in points:
+        regr_dict = dict(zip(col_names, regr_output[:, r, c]))
+        coords.append(_regr_back(regr_dict))
+        coords[-1]['confidence'] = 1 / (1 + np.exp(-logits[r, c]))
+        coords[-1]['x'], coords[-1]['y'], coords[-1]['z'] = \
+            optimize_xy(r, c,
+                        coords[-1]['x'],
+                        coords[-1]['y'],
+                        coords[-1]['z'], flipped)
+    coords = clear_duplicates(coords)
+    return coords
+
+
+def coords2str(coords, names=['yaw', 'pitch', 'roll', 'x', 'y', 'z', 'confidence']):
+    s = []
+    for c in coords:
+        for n in names:
+            s.append(str(c.get(n, 0)))
+    return ' '.join(s)
+
+
+class CarDataset(Dataset):
+    """Car dataset."""
+
+    def __init__(self, dataframe, root_dir, training=True, transform=None):
+        self.df = dataframe
+        self.root_dir = root_dir
+        self.transform = transform
+        self.training = training
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        # Get image name
+        idx, labels = self.df.values[idx]
+        img_name = self.root_dir.format(idx)
+
+        # Augmentation
+        flip = False
+        if self.training:
+            flip = np.random.randint(10) == 1
+
+        # Read image
+        img0 = imread(img_name, True)
+        img = preprocess_image(img0, flip=flip)
+        img = np.rollaxis(img, 2, 0)
+
+        # Get mask and regression maps
+        mask, regr = get_mask_and_regr(img0, labels, flip=flip)
+        regr = np.rollaxis(regr, 2, 0)
+
+        return [img, mask, regr]
+
+
+train_images_dir = PATH + 'train_images/{}.jpg'
+test_images_dir = PATH + 'test_images/{}.jpg'
+
+df_train, df_val = train_test_split(train, test_size=0.01, random_state=42)
+df_test = test
+
+# Create dataset objects
+train_dataset = CarDataset(df_train, train_images_dir, training=True)
+val_dataset = CarDataset(df_val, train_images_dir, training=False)
+test_dataset = CarDataset(df_test, test_images_dir, training=False)
+
+
+# Create data generators - they will produce batches
+train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+dev_loader = DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
+test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
